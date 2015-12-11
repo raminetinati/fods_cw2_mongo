b2b36ec4c265cfa4564db1ec34feb64cc2021605
Antonios Andronis <aa3e15@soton.ac.uk>
2015-12-11 22:56:04 +0000

    adding python2.7 on requirements

--- scripts/install_system_dependencies.sh
+++ scripts/install_system_dependencies.sh
@@ -2,5 +2,5 @@
 
 if [[ $(uname) == "Linux" ]]; then
 	# TODO either  make generic for all linuxes or remove
-	sudo apt-get install python-virtualenv python-pip libmysqlclient-dev
+	sudo apt-get install python2.7 python-virtualenv python-pip libmysqlclient-dev
 fi
